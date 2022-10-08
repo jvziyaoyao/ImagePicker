455453d9cd873fa30bba3e2f8478fc3b4dc75e56
shijie.zhong <shijie.zhong@hiscene.com>
2022-10-08 17:47:25 +0800

    fix: 修复大图加载很卡的问题

--- imagePicker/src/main/java/com/origeek/imagePicker/ui/ImageLoader.kt
+++ imagePicker/src/main/java/com/origeek/imagePicker/ui/ImageLoader.kt
@@ -2,10 +2,10 @@ package com.origeek.imagePicker.ui
 
 import android.graphics.BitmapRegionDecoder
 import android.os.Build.VERSION.SDK_INT
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.GifDecoder
@@ -16,6 +16,9 @@ import coil.size.Size
 import com.origeek.imagePicker.R
 import com.origeek.imagePicker.util.ContextUtil
 import com.origeek.imageViewer.ImageDecoder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 // 图片加载器
 val imageLoader = ImageLoader.Builder(ContextUtil.getApplicationByReflect())
@@ -46,16 +49,24 @@ fun rememberCoilImagePainter(
     return rememberAsyncImagePainter(model = imageRequest, imageLoader = imageLoader)
 }
 
+
 @Composable
-fun rememberHugeImagePainter(path: String): ImageDecoder? {
-    return remember {
-        try {
-            val decoder = BitmapRegionDecoder.newInstance(path, false)
-            val imageDecoder = ImageDecoder(decoder = decoder)
-            imageDecoder
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
+fun rememberHugeImagePainter(path: String): Any? {
+    val defaultPainter = painterResource(id = R.drawable.ic_empty_image)
+    var painter by remember { mutableStateOf<Any?>(defaultPainter) }
+    LaunchedEffect(path) {
+        launch(Dispatchers.IO) {
+            val imageDecoder = try {
+                val decoder = BitmapRegionDecoder.newInstance(path, false)
+                ImageDecoder(decoder = decoder)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+            launch(Dispatchers.Main) {
+                painter = imageDecoder
+            }
         }
     }
+    return painter
 }
